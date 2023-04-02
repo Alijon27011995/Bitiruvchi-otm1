26e3485a2d56eb8b586321a7b40cecaa97266b32
Alijon <muxorovalijon051@gmail.com>
2023-04-02 13:23:32 +0500

    dwaed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name' 60);
-            $table->string('email' 100);
-            $table->unum('gender',["M","F","O"]);
-            $table->string('address');
-            $table->date('dob');
-            $table->string('password');
-            $table->timestamp('email_verified_at')->nullable();
-            
             $table->rememberToken();
             $table->timestamps();
-        });     
+        });
     }
 
     /**
